634130638c657242d8b9b388a7b70a887e7cb938
contaempresa243-eng <contaempresa243@gmail.com>
2026-09-17 11:55:51 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'saldar-servicos-cache-v17';
+const CACHE_NAME = 'saldar-servicos-cache-v18';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.json'];
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
